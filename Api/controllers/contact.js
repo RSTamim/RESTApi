ed31006b5da26f374ffb3a1e7f0efb80ec7d8c02
Md. Tamim Hasan <59114274+RSTamim@users.noreply.github.com>
2022-11-28 00:40:10 +0600

    Update contact.js

--- Api/controllers/contact.js
+++ Api/controllers/contact.js
@@ -1,5 +1,5 @@
 const Contact = require("../models/Contact");
-const Contact = require("../models/
+const Contact = require("../mod
 
 const getAllContactController = (req, res, next) => {
   Contact.find()
