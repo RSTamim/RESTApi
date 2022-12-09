e63c4c92e1bc9286b63782550ab3095d71ce176f
Md. Tamim Hasan <59114274+RSTamim@users.noreply.github.com>
2022-12-09 14:31:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cors = require("cors");
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/contacts-DB");
-mongoose.connect("mongodb://localhost:27017/contacts-DB");
+mongoose.connect("mongodb://localhost:27017/contacts-
 
 
 
